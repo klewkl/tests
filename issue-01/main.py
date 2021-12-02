7dd93a5b482d32335512e66db5483b9bc411154f
klewkl <77967173+klewkl@users.noreply.github.com>
2021-12-02 10:10:43 +0300

    Update main.py

--- issue-01/main.py
+++ issue-01/main.py
@@ -1,1 +1,60 @@
+import doctest
 
+"""Morse Code Translator"""
+
+LETTER_TO_MORSE = {
+    'A': '.-', 'B': '-...', 'C': '-.-.',
+    'D': '-..', 'E': '.', 'F': '..-.',
+    'G': '--.', 'H': '....', 'I': '..',
+    'J': '.---', 'K': '-.-', 'L': '.-..',
+    'M': '--', 'N': '-.', 'O': '---',
+    'P': '.--.', 'Q': '--.-', 'R': '.-.',
+    'S': '...', 'T': '-', 'U': '..-',
+    'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..', '1': '.----',
+    '2': '..---', '3': '...--', '4': '....-',
+    '5': '.....', '6': '-....', '7': '--...',
+    '8': '---..', '9': '----.', '0': '-----',
+    ', ': '--..--', '.': '.-.-.-', '?': '..--..',
+    '/': '-..-.', '-': '-....-', '(': '-.--.', ')': '-.--.-',
+    ' ': ' '
+}
+
+MORSE_TO_LETTER = {
+    morse: letter
+    for letter, morse in LETTER_TO_MORSE.items()
+}
+
+
+def encode(message: str) -> str:
+    """
+    Кодирует строку в соответсвие с таблицей азбуки Морзе
+    """
+    encoded_signs = [
+        LETTER_TO_MORSE[letter] for letter in message
+    ]
+
+    return ' '.join(encoded_signs)
+
+
+def decode(morse_message: str) -> str:
+    """
+   >>> encode('SOS') #doctest: +NORMALIZE_WHITESPACE
+   '... --- ...'
+    >>> encode('ANNA') #doctest: +NORMALIZE_WHITESPACE
+    '.- -. -. .-'
+    >>> list(encode('A'*5)) # doctest: +ELLIPSIS
+    ['.', '-', ' ', '.', '-', ' ', ... ' ', '.', '-', ' ', '.', '-']
+    >>> encode(21)
+    Traceback (most recent call last):
+    TypeError: 'int' object is not iterable
+       """
+    decoded_letters = [
+        MORSE_TO_LETTER[letter] for letter in morse_message.split()
+    ]
+
+    return ''.join(decoded_letters)
+
+
+if __name__ == '__main__':
+    doctest.testmod()
